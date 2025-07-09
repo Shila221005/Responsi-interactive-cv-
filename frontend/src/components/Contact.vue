a6c565229ba6837e97efe35d66174ad5f31cd31c
Shila221005 <chintyasheila22@gmail.com>
2025-07-10 03:05:07 +0700

    Selesai tinggal perbaikan

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -5,9 +5,9 @@ import SectionTitle from './SectionTitle.vue'
   <footer id="kontak" class="py-20 bg-gray-800 text-white">
     <div class="container mx-auto px-6 text-center">
       <SectionTitle title="Hubungi Saya" />
-      <p class="text-xl text-gray-300 mb-8">Jangan ragu untuk terhubung atau bertanya.</p>
+      <p class="text-xl text-gray-300 mb-8">Jangan ragu untuk menghubungi atau bertanya.</p>
       <a
-        href="mailto:emailanda@example.com"
+        href="chintyasheila22@gmail.com"
         class="bg-blue-600 text-white font-bold py-3 px-6 rounded-lg hover:bg-blue-700 transition-colors duration-300 inline-block mb-12"
       >
         Kirim Email
@@ -18,6 +18,7 @@ import SectionTitle from './SectionTitle.vue'
           target="_blank"
           class="text-gray-400 hover:text-white transition-colors duration 300"
           >GitHub</a
+
         >
         <a
           href="#"
@@ -27,7 +28,7 @@ import SectionTitle from './SectionTitle.vue'
         >
       </div>
       <p class="text-gray-500">
-        &copy; {{ new Date().getFullYear() }} Nama Mahasiswa. Dibuat dengan
+        &copy; {{ new Date().getFullYear() }} Chintya Sheila Masterina. Dibuat dengan
       </p>
     </div>
   </footer>
--- frontend/src/components/Hero.vue
+++ frontend/src/components/Hero.vue
@@ -1,10 +1,10 @@
 <template>
   <section id="profil" class="container mx-auto px-6 py-20 flex flex-col md:flex-row items-center">
     <div class="md:w-1/2 mb-10 md:mb-0">
-      <h1 class="text-5xl font-bold text-gray-800 mb-4 animate-slide-in-left" :style="{ 'animation-delay': '200ms' }">
+      <h1 class="text-6xl font-bold text-gray-800 mb-4 animate-slide-in-left" :style="{ 'animation-delay': '200ms' }">
         Halo, Saya <span class="text-blue-600">Chintya Sheila Masterina</span>
       </h1>
-      <p class="text-xl text-gray-600 mb-8 animate-slide-in-left" :style="{ 'animation-delay': '400ms' }">
+      <p class="text-2xl text-gray-600 mb-8 animate-slide-in-left" :style="{ 'animation-delay': '400ms' }">
         Mahasiswa Informatika yang bersemangat dalam pengembangan web dan desain antarmuka.
       </p>
       <a href="#kontak"
@@ -14,7 +14,7 @@
       </a>
     </div>
     <div class="md:w-1/2 flex justify-center animate-slide-in-right">
-      <img src="@/assets/profil.jpg" alt="Foto Profil"
+      <img src="../assets/FotoProfil.jpg" alt="Foto Profil"
         class="w-80 h-80 rounded-full object-cover shadow-lg">
     </div>
   </section>
